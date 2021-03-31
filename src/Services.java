898ae3159eb958dd60f928fcc6f47e24fdfc82a2
Bara C. Tudor <vector2025@gmail.com>
2021-03-31 21:13:58 +0300

    add some queries

--- src/Services.java
+++ src/Services.java
@@ -17,8 +17,19 @@
  * - user now has bought item
  */
 
-public class Services {
-    public static void winningUserFromAuction(){
+import java.util.stream.Stream;
 
+public class Services {
+    public static void totalMoneySpendByUser(User user){
+        var total = user.getItemsBought().stream().map(Item::getStartingPrice).reduce(0, Integer::sum);
+        System.out.println("user " + user.getName() + " spent in total: " + total);
+    }
+    public static void totalMoneySpendInCategoryOfItems(Category category){
+        var total = category.getItems().stream().map(Item::getStartingPrice).reduce(0, Integer::sum);
+        System.out.println("money spent in the category " + category.getName() + " is " + total);
+    }
+    public static void averageMoneySpendInCategoryOfItems(Category category){
+        var total = category.getItems().stream().map(Item::getStartingPrice).reduce(0, Integer::sum);
+        System.out.println("people spend in averege " + total + " dollars in " + category.getName());
     }
 }
