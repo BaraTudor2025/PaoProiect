c916151babd3c00ef941f8c54fc3fbcb730e4594
Bara C. Tudor <vector2025@gmail.com>
2021-03-31 13:55:16 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        // licitatii=event
+        // bids=suma
+        // produse=ce se liciteaza
+        // utilizator=cel ce liciteaza(buyer, seller, user)
+        //
+        //
+    }
+}
